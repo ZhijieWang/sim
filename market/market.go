142f0e6a5f4936c21c78797976de34f2d8213f72
zhijie wang <zhijie.wang@accenture.com>
2020-05-19 15:30:28 -0700

    remove market actor stuff for now:

--- market/market.go
+++ market/market.go
@@ -11,6 +11,23 @@ var seededRand *rand.Rand = rand.New(
 
 const charset = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
+type Quote struct {
+	LastTradedPrice  float64
+	LastTradedVolume int
+	CurrentBid       float64
+	BidVolumer       int
+	CurrentAsk       float64
+	AskVolume        int
+}
+
+type Market interface {
+	//OrderHandler(Order) bool
+	GetSymbol() string
+	PlaceOrder(orderbook.Order) []orderbook.Trade
+	GetQuote() Quote
+	init(float64, int)
+}
+
 func StringWithCharset(length int, charset string) string {
 	b := make([]byte, length)
 	for i := range b {
@@ -32,13 +49,56 @@ type marketImpl struct {
 }
 
 func NewMarket() Market {
-	return &marketImpl{}
+	m := &marketImpl{
+		stock:      String(10),
+		shares:     1000,
+		lastPrice:  0,
+		lastVolume: 0,
+		book:       nil,
+	}
+	m.init(1, 1000)
+	return m
 }
 
 // init function implements the IPO process of a stock.
-func (m *marketImpl) init(price float32, quantity int) {
-	m.book = orderbook.NewOrderBook(price, quantity)
+func (m *marketImpl) init(price float64, quantity int) {
+	m.book = orderbook.NewOrderBook()
+	m.book.PlaceAsk(orderbook.NewOrder(orderbook.BidOrder, price, quantity, m.stock))
 }
 func (m *marketImpl) GetSymbol() string {
 	return m.stock
+}
+func (m *marketImpl) GetQuote() Quote {
+	curBidP, curBidV := m.book.GetCurrentBid()
+	curAskP, curAskV := m.book.GetCurrentAsk()
+	return Quote{
+		m.lastPrice,
+		m.lastVolume,
+		curBidP,
+		curBidV,
+		curAskP,
+		curAskV,
+	}
+}
+
+func (m *marketImpl) PlaceOrder(order orderbook.Order) []orderbook.Trade {
+	var trades []orderbook.Trade
+	switch order.GetType() {
+	case orderbook.AskOrder:
+		_, trades = m.book.PlaceAsk(order)
+
+	case orderbook.BidOrder:
+		_, trades = m.book.PlaceBid(order)
+	}
+	if len(trades) != 0 {
+		// trade made, need to update last price and volume
+		m.lastPrice = trades[len(trades)-1].GetFilledAvgPrice()
+		m.lastVolume = 0
+		for _, t := range trades {
+			m.lastVolume += t.GetFilledQuantity()
+		}
+		m.lastVolume /= 2
+		// the trades come in pair (1 buy 1 sell). The volume should be executed.
+	}
+	return trades
 }
--- market/market_test.go
+++ market/market_test.go
@@ -1,7 +1,31 @@
-package main
+package market
 
-import "testing"
+import (
+	"marketplace/orderbook"
+	"math"
+	"testing"
 
-func TestHelloWorld(t *testing.T) {
-	// t.Fatal("not implemented")
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMarketInitialization(t *testing.T) {
+	m := NewMarket()
+	q := m.GetQuote()
+	assert.Equal(t, 0, q.LastTradedVolume)
+	assert.Equal(t, 0.0, q.LastTradedPrice)
+	assert.Equal(t, 0.0, q.CurrentBid)
+	assert.Equal(t, 1.0, q.CurrentAsk)
+}
+
+func TestOrderExecution(t *testing.T) {
+
+	m := NewMarket()
+	trades := m.PlaceOrder(orderbook.NewOrder(orderbook.BidOrder, 1.0, 1000, ""))
+	assert.Equal(t, 2, len(trades))
+	assert.Equal(t, 1000, trades[0].GetFilledQuantity())
+	q := m.GetQuote()
+	assert.Equal(t, 1000, q.LastTradedVolume)
+	assert.Equal(t, 1.0, q.LastTradedPrice)
+	assert.Equal(t, math.Inf(1), q.CurrentAsk)
+	assert.Equal(t, 0.0, q.CurrentBid)
 }
